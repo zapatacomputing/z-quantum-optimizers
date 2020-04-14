d1d3ea6aeffdeeff37d57b3b4c9cfd5dfa465c44
michalstechly <michal.stechly@gmail.com>
2020-04-14 16:49:55 -0400

    Removed unnecessary comments.

--- src/python/zquantum/optimizers/variational_circuits.py
+++ src/python/zquantum/optimizers/variational_circuits.py
@@ -15,11 +15,11 @@ def optimize_variational_circuit(ansatz, operator, initial_params,
         def cost_function(params):
             # Build the ansatz circuit
             circuit = build_ansatz_circuit(ansatz, params)
-            # TODO: leave it as it is or change?
+
             operator_no_coeff = copy.deepcopy(target_operator)
             for term in target_operator.terms:
                 operator_no_coeff.terms[term] = 1
-
+    
             expectation_values = backend.get_expectation_values(circuit, operator_no_coeff)
             value_estimate = evaluate_qubit_operator(target_operator, expectation_values)
             return value_estimate.value
