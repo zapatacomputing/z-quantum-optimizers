d3b502a8e863abf1ca10824f121330579d3c7f79
Max Radin <radin.max@gmail.com>
2020-09-25 11:11:03 -0400

    Adding setup.py to root

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import os
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="z-quantum-optimizers",
+    version="0.2.0",
+    author="Zapata Computing, Inc.",
+    author_email="info@zapatacomputing.com",
+    description="Core optimizers for Orquestra.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zapatacomputing/z-quantum-optimizers ",
+    packages=setuptools.find_namespace_packages(include=["zquantum.*"]),
+    package_dir={"": "src/python"},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        "z-quantum-core",
+        "pytest>=5.3.5",
+        "marshmallow>=3.4.0",
+        "cma==2.7.0",
+        "Werkzeug>=1.0.0",
+        "flask>=1.1.2",
+    ],
+)
