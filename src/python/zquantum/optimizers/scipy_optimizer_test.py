ee508a0d5c3a54133a067afc78275a4b8c0292a0
dexter2206 <dexter2206@gmail.com>
2020-09-29 13:21:02 +0200

    Fix tests to work with current implementation of cost function and history recording

--- src/python/zquantum/optimizers/scipy_optimizer_test.py
+++ src/python/zquantum/optimizers/scipy_optimizer_test.py
@@ -23,14 +23,12 @@ class ScipyOptimizerTests(unittest.TestCase, OptimizerTests):
 
     def test_SLSQP_with_equality_constraints(self):
         # Given
-        cost_function = BasicCostFunction(
-            rosenbrock_function, gradient_type="finite_difference"
+        cost_function = FunctionWithGradient(
+            rosenbrock_function, finite_differences_gradient(rosenbrock_function)
         )
-        constraint_cost_function = BasicCostFunction(sum_x_squared)
-        constraint_cost_function_wrapper = lambda params: constraint_cost_function.evaluate(
-            params
-        ).value
-        constraints = ({"type": "eq", "fun": constraint_cost_function_wrapper},)
+        constraint_cost_function = sum_x_squared
+
+        constraints = ({"type": "eq", "fun": constraint_cost_function},)
         optimizer = ScipyOptimizer(method="SLSQP", constraints=constraints)
         initial_params = np.array([1, 1])
         target_params = np.array([0, 0])
@@ -47,8 +45,8 @@ class ScipyOptimizerTests(unittest.TestCase, OptimizerTests):
 
     def test_SLSQP_with_inequality_constraints(self):
         # Given
-        cost_function = BasicCostFunction(
-            rosenbrock_function, gradient_type="finite_difference"
+        cost_function = FunctionWithGradient(
+            rosenbrock_function, finite_differences_gradient(rosenbrock_function)
         )
         constraints = {"type": "ineq", "fun": lambda x: x[0] + x[1] - 3}
         optimizer = ScipyOptimizer(method="SLSQP")
