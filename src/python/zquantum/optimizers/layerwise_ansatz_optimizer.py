8e068b6e16807b4b01a890c8e280576ac02f88ef
mstechly <michal.stechly@gmail.com>
2021-02-05 21:33:41 -0500

    Prototype of lbl optimizer.

---
+++ src/python/zquantum/optimizers/layerwise_ansatz_optimizer.py
@@ -0,0 +1,85 @@
+from zquantum.core.history.recorder import recorder
+from zquantum.core.interfaces.functions import CallableWithGradient
+from zquantum.core.interfaces.optimizer import Optimizer, optimization_result
+from zquantum.core.interfaces.ansatz import Ansatz
+from zquantum.core.interfaces.backend import QuantumBackend
+from zquantum.core.interfaces.functions import CallableStoringArtifacts
+from scipy.optimize import OptimizeResult
+from typing import Dict, Optional, List
+import numpy as np
+import random
+
+
+class LayerwiseAnsatzOptimizer:
+    def __init__(
+        self,
+        inner_optimizer: Optimizer,
+        options: Optional[Dict] = None,
+    ):
+        """
+        Args:
+            grid(from zquantum.core.circuit.ParameterGrid): object defining for which parameters we want do the evaluations
+            options(dict): dictionary with additional options for the optimizer.
+
+        Supported values for the options dictionary:
+        Options:
+            keep_value_history(bool): boolean flag indicating whether the history of evaluations should be stored or not.
+
+        """
+        if options is None:
+            options = {}
+        self.options = options
+        self.inner_optimizer = inner_optimizer
+        if "keep_value_history" not in self.options.keys():
+            self.keep_value_history = False
+        else:
+            self.keep_value_history = self.options["keep_value_history"]
+            del self.options["keep_value_history"]
+
+    def minimize_lbl(
+        self,
+        cost_function: CallableStoringArtifacts,
+        min_layer: int,
+        max_layer: int,
+        params_min_values: List[float],
+        params_max_values: List[float],
+    ) -> OptimizeResult:
+        """
+        Finds the parameters which minimize given cost function, by trying all the parameters from the grid.
+
+        Args:
+            cost_function(zquantum.core.interfaces.cost_function.CostFunction): object representing cost function we want to minimize
+            inital_params (np.ndarray): initial parameters for the cost function
+
+        Returns:
+            OptimizeResults
+        """
+
+        if self.keep_value_history:
+            cost_function = recorder(cost_function)
+
+        cost_function.ansatz.number_of_layers = min_layer
+
+        number_of_params = cost_function.ansatz.number_of_params
+        initial_params = [
+            params_min_values[i]
+            + random.random() * (params_max_values[i] - params_min_values[i])
+            for i in range(number_of_params)
+        ]
+
+        initial_params = np.array(initial_params)
+
+        for _ in range(min_layer, max_layer):
+            layer_results = self.inner_optimizer.minimize(cost_function, initial_params)
+            optimal_params = layer_results.opt_params
+            new_layer_params = [
+                params_min_values[i]
+                + random.random() * (params_max_values[i] - params_min_values[i])
+                for i in range(2)
+            ]
+            initial_params = np.array(optimal_params.tolist() + new_layer_params)
+            cost_function.ansatz.number_of_layers += 1
+
+        cost_function.ansatz.number_of_layers = max_layer
+
+        return layer_results
---
+++ steps/lbl_optimizers.py
@@ -0,0 +1,123 @@
+from zquantum.core.circuit import (
+    load_circuit_template_params,
+    save_circuit_template_params,
+    load_parameter_grid,
+    load_circuit_connectivity,
+)
+from zquantum.core.openfermion import load_qubit_operator
+from zquantum.core.utils import create_object, load_noise_model
+from zquantum.core.serialization import (
+    save_optimization_results,
+    load_optimization_results,
+)
+
+from zquantum.optimizers import LayerwiseAnsatzOptimizer
+
+import yaml
+import numpy as np
+import os
+import json
+
+
+def repeated_optimize_variational_circuit_with_layerwise_optimizer(
+    ansatz_specs,
+    backend_specs,
+    optimizer_specs,
+    cost_function_specs,
+    qubit_operator,
+    max_layer,
+    params_min_values,
+    params_max_values,
+    number_of_repeats,
+):
+    for i in range(number_of_repeats):
+        optimize_variational_circuit_with_layerwise_optimizer(
+            ansatz_specs,
+            backend_specs,
+            optimizer_specs,
+            cost_function_specs,
+            qubit_operator,
+            max_layer,
+            params_min_values,
+            params_max_values,
+        )
+        os.rename("optimization-results.json", f"optimization-results-{i}.json")
+        os.rename("optimized-parameters.json", f"optimized-parameters-{i}.json")
+
+    final_results_list = []
+    final_parameters_list = []
+    for i in range(number_of_repeats):
+        results_file = file("optimization-results-{i}.json", "r")
+        parameters_file = file("optimized-parameters-{i}.json", "r")
+        final_results_list.append(yaml.load(results_file, Loader=yaml.SafeLoader))
+        final_parameters_list.append(yaml.load(parameters_file, Loader=yaml.SafeLoader))
+
+    with open("optimization-results-list.json", "w") as outfile:
+        json.dump(final_results_list, outfile)
+    with open("optimized-parameters-list.json", "w") as outfile:
+        json.dump(final_parameters_list, outfile)
+
+
+def optimize_variational_circuit_with_layerwise_optimizer(
+    ansatz_specs,
+    backend_specs,
+    optimizer_specs,
+    cost_function_specs,
+    qubit_operator,
+    max_layer,
+    params_min_values,
+    params_max_values,
+):
+    # Load qubit operator
+    operator = load_qubit_operator(qubit_operator)
+
+    if isinstance(ansatz_specs, str):
+        ansatz_specs_dict = yaml.load(ansatz_specs, Loader=yaml.SafeLoader)
+    else:
+        ansatz_specs_dict = ansatz_specs
+    if ansatz_specs_dict["function_name"] == "QAOAFarhiAnsatz":
+        ansatz = create_object(ansatz_specs_dict, cost_hamiltonian=operator)
+    else:
+        ansatz = create_object(ansatz_specs_dict)
+
+    # Load optimizer specs
+    if isinstance(optimizer_specs, str):
+        optimizer_specs_dict = yaml.load(optimizer_specs, Loader=yaml.SafeLoader)
+    else:
+        optimizer_specs_dict = optimizer_specs
+    optimizer = create_object(optimizer_specs_dict, ansatz=ansatz)
+
+    # Load backend specs
+    if isinstance(backend_specs, str):
+        backend_specs_dict = yaml.load(backend_specs, Loader=yaml.SafeLoader)
+    else:
+        backend_specs_dict = backend_specs
+
+    backend = create_object(backend_specs_dict)
+
+    # Load cost function specs
+    if isinstance(cost_function_specs, str):
+        cost_function_specs_dict = yaml.load(
+            cost_function_specs, Loader=yaml.SafeLoader
+        )
+    else:
+        cost_function_specs_dict = cost_function_specs
+    estimator_specs = cost_function_specs_dict.pop("estimator-specs", None)
+    if estimator_specs is not None:
+        cost_function_specs_dict["estimator"] = create_object(estimator_specs)
+    cost_function_specs_dict["target_operator"] = operator
+    cost_function_specs_dict["ansatz"] = ansatz
+    cost_function_specs_dict["backend"] = backend
+    cost_function = create_object(cost_function_specs_dict)
+
+    lbl_optimizer = LayerwiseAnsatzOptimizer(inner_optimizer=optimizer)
+    opt_results = lbl_optimizer.minimize_lbl(
+        cost_function,
+        min_layer=1,
+        max_layer=max_layer,
+        params_min_values=params_min_values,
+        params_max_values=params_max_values,
+    )
+
+    save_optimization_results(opt_results, "optimization-results.json")
+    save_circuit_template_params(opt_results.opt_params, "optimized-parameters.json")
