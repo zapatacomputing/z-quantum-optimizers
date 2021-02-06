35a1e75ebba2649e1404d33d2fd09bbe33c987dc
mstechly <michal.stechly@gmail.com>
2021-02-05 21:59:18 -0500

    fixup! Prototype of lbl optimizer.

--- src/python/zquantum/optimizers/__init__.py
+++ src/python/zquantum/optimizers/__init__.py
@@ -1,3 +1,4 @@
 from .grid_search import GridSearchOptimizer
 from .scipy_optimizer import ScipyOptimizer
 from .cma_es_optimizer import CMAESOptimizer
+from .layerwise_ansatz_optimizer import LayerwiseAnsatzOptimizer
