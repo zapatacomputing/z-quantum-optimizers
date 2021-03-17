c014f6cc1060030973959e0e0d6df008341a3767
mstechly <michal.stechly@gmail.com>
2021-03-17 14:25:00 -0400

    Removed problematic import.

--- steps/lbl_optimizers.py
+++ steps/lbl_optimizers.py
@@ -13,12 +13,6 @@ from zquantum.core.serialization import (
 
 from zquantum.optimizers import LayerwiseAnsatzOptimizer
 
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(".").absolute()))
-
-from optimizers import optimize_variational_circuit
 import yaml
 import numpy as np
 import os
@@ -170,3 +164,116 @@ def optimize_variational_circuit_with_layerwise_optimizer(
     save_optimization_results(final_results, "optimization-results.json")
     save_circuit_template_params(final_results.opt_params, "optimized-parameters.json")
     return opt_results
+
+
+def optimize_variational_circuit(
+    ansatz_specs,
+    backend_specs,
+    optimizer_specs,
+    cost_function_specs,
+    qubit_operator,
+    initial_parameters="None",
+    fixed_parameters="None",
+    noise_model="None",
+    device_connectivity="None",
+    parameter_grid="None",
+    constraint_operator="None",
+):
+    if initial_parameters != "None":
+        initial_params = load_circuit_template_params(initial_parameters)
+    else:
+        initial_params = None
+
+    if fixed_parameters != "None":
+        fixed_params = load_circuit_template_params(fixed_parameters)
+    else:
+        fixed_params = None
+
+    # Load qubit operator
+    operator = load_qubit_operator(qubit_operator)
+
+    if isinstance(ansatz_specs, str):
+        ansatz_specs_dict = yaml.load(ansatz_specs, Loader=yaml.SafeLoader)
+    else:
+        ansatz_specs_dict = ansatz_specs
+    if ansatz_specs_dict["function_name"] == "QAOAFarhiAnsatz":
+        ansatz = create_object(ansatz_specs_dict, cost_hamiltonian=operator)
+    else:
+        ansatz = create_object(ansatz_specs_dict)
+
+    # Load parameter grid
+    if parameter_grid != "None":
+        grid = load_parameter_grid(parameter_grid)
+    else:
+        grid = None
+
+    # Load optimizer specs
+    if isinstance(optimizer_specs, str):
+        optimizer_specs_dict = yaml.load(optimizer_specs, Loader=yaml.SafeLoader)
+    else:
+        optimizer_specs_dict = optimizer_specs
+    if (
+        grid is not None
+        and optimizer_specs_dict["function_name"] == "GridSearchOptimizer"
+    ):
+        optimizer = create_object(optimizer_specs_dict, grid=grid)
+    else:
+        optimizer = create_object(optimizer_specs_dict)
+
+    # Load backend specs
+    if isinstance(backend_specs, str):
+        backend_specs_dict = yaml.load(backend_specs, Loader=yaml.SafeLoader)
+    else:
+        backend_specs_dict = backend_specs
+    if noise_model != "None":
+        backend_specs_dict["noise_model"] = load_noise_model(noise_model)
+    if device_connectivity != "None":
+        backend_specs_dict["device_connectivity"] = load_circuit_connectivity(
+            device_connectivity
+        )
+    backend = create_object(backend_specs_dict)
+
+    # Load cost function specs
+    if isinstance(cost_function_specs, str):
+        cost_function_specs_dict = yaml.load(
+            cost_function_specs, Loader=yaml.SafeLoader
+        )
+    else:
+        cost_function_specs_dict = cost_function_specs
+    estimator_specs = cost_function_specs_dict.pop("estimator-specs", None)
+    if estimator_specs is not None:
+        cost_function_specs_dict["estimator"] = create_object(estimator_specs)
+    cost_function_specs_dict["target_operator"] = operator
+    cost_function_specs_dict["ansatz"] = ansatz
+    cost_function_specs_dict["backend"] = backend
+    cost_function_specs_dict["fixed_parameters"] = fixed_params
+    cost_function = create_object(cost_function_specs_dict)
+
+    if constraint_operator != "None":
+        constraint_op = load_qubit_operator(constraint_operator)
+        constraints_cost_function_specs = yaml.load(
+            cost_function_specs, Loader=yaml.SafeLoader
+        )
+        constraints_estimator_specs = constraints_cost_function_specs.pop(
+            "estimator-specs", None
+        )
+        if constraints_estimator_specs is not None:
+            constraints_cost_function_specs["estimator"] = create_object(
+                constraints_estimator_specs
+            )
+        constraints_cost_function_specs["ansatz"] = ansatz
+        constraints_cost_function_specs["backend"] = backend
+        constraints_cost_function_specs["target_operator"] = constraint_op
+        constraint_cost_function = create_object(constraints_cost_function_specs)
+        constraint_cost_function_wrapper = (
+            lambda params: constraint_cost_function.evaluate(params).value
+        )
+        constraint_functions = (
+            {"type": "eq", "fun": constraint_cost_function_wrapper},
+        )
+        optimizer.constraints = constraint_functions
+
+    opt_results = optimizer.minimize(cost_function, initial_params)
+
+    save_optimization_results(opt_results, "optimization-results.json")
+    save_circuit_template_params(opt_results.opt_params, "optimized-parameters.json")
