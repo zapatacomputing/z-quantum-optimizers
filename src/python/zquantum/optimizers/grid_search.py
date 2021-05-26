407fc528a546e0c835c8fa9e7c269ebb9ad2e2e6
mstechly <michal.stechly@gmail.com>
2021-05-26 16:29:51 -0400

    Moved ParameterGrid from core.

--- src/python/zquantum/optimizers/grid_search.py
+++ src/python/zquantum/optimizers/grid_search.py
@@ -1,23 +1,92 @@
 from zquantum.core.history.recorder import recorder
 from zquantum.core.interfaces.functions import CallableWithGradient
 from zquantum.core.interfaces.optimizer import Optimizer, optimization_result
-from zquantum.core.circuit import ParameterGrid
 from scipy.optimize import OptimizeResult
-from typing import Dict, Optional
+from typing import Dict, Optional, List, Tuple
 import numpy as np
+
+
+class ParameterGrid:
+    """A class representing a grid of parameter values to be used in a grid search.
+
+    Args:
+        param_ranges: ranges of the parameters describing the shape of the grid.
+            Each range consist is of the form (min, max, step).
+
+    Attributes:
+        param_ranges (list): same as above.
+    """
+
+    def __init__(self, param_ranges: List[Tuple[float, float, float]]):
+        self.param_ranges = param_ranges
+
+    @property
+    def params_list(self) -> List[np.ndarray]:
+        grid_array = np.reshape(np.stack(self.params_meshgrid), (self.n_params, -1))
+
+        return [grid_array[:, i].flatten() for i in range(grid_array.shape[1])]
+
+    def to_dict(self) -> dict:
+        return {"param_ranges": self.param_ranges}
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(data["param_ranges"])
+
+    @property
+    def params_meshgrid(self) -> Tuple[np.ndarray, ...]:
+        """
+        Creates a meshgrid from the parameter ranges.
+        """
+        param_vectors: List[np.ndarray] = []
+
+        for param_spec in self.param_ranges:
+            param_vectors.append(np.arange(param_spec[0], param_spec[1], param_spec[2]))
+
+        return np.meshgrid(*param_vectors, indexing="ij")
+
+    @property
+    def n_params(self) -> int:
+        return len(self.param_ranges)
+
+
+def build_uniform_param_grid(
+    n_params_per_layer: int,
+    n_layers: int = 1,
+    min_value: float = 0.0,
+    max_value: float = 2 * np.pi,
+    step: float = np.pi / 5,
+) -> ParameterGrid:
+    """Builds a uniform grid of parameters.
+
+    Args:
+        n_params_per_layer: number of parameters for each layer
+        n_layers: the number of layers to create parameters for
+        min_value: the minimum value for the parameters
+        max_value: the maximum value for the parameters
+        step: the step size
+
+    Returns:
+        Points on a grid in parameter space.
+    """
+
+    n_params = n_params_per_layer * n_layers
+
+    param_ranges = [(min_value, max_value, step)] * n_params
+    return ParameterGrid(param_ranges)
 
 
 class GridSearchOptimizer(Optimizer):
     def __init__(self, grid: ParameterGrid, options: Optional[Dict] = None):
         """
         Args:
-            grid(from zquantum.core.circuit.ParameterGrid): object defining for which parameters we want do the evaluations
-            options(dict): dictionary with additional options for the optimizer.
+            grid: object defining for which parameters we want do the evaluations
+            options: dictionary with additional options for the optimizer.
 
         Supported values for the options dictionary:
         Options:
-            keep_value_history(bool): boolean flag indicating whether the history of evaluations should be stored or not.
-            
+            keep_value_history: boolean flag indicating whether the history of evaluations should be stored or not.
+
         """
         if options is None:
             options = {}
@@ -30,17 +99,16 @@ class GridSearchOptimizer(Optimizer):
             del self.options["keep_value_history"]
 
     def minimize(
-        self, cost_function: CallableWithGradient, initial_params: Optional[np.ndarray] = None
+        self,
+        cost_function: CallableWithGradient,
+        initial_params: Optional[np.ndarray] = None,
     ) -> OptimizeResult:
         """
         Finds the parameters which minimize given cost function, by trying all the parameters from the grid.
 
         Args:
-            cost_function(zquantum.core.interfaces.cost_function.CostFunction): object representing cost function we want to minimize
-            inital_params (np.ndarray): initial parameters for the cost function
-
-        Returns:
-            OptimizeResults
+            cost_function: object representing cost function we want to minimize
+            inital_params: initial parameters for the cost function
         """
         if initial_params is not None and len(initial_params) != 0:
             Warning("Grid search doesn't use initial parameters, they will be ignored.")
@@ -58,17 +126,20 @@ class GridSearchOptimizer(Optimizer):
                 min_value = value
                 optimal_params = params
 
-
         return optimization_result(
-            opt_value=min_value, opt_params=optimal_params, nfev=nfev, nit=None, history=cost_function.history if self.keep_value_history else []
+            opt_value=min_value,
+            opt_params=optimal_params,
+            nfev=nfev,
+            nit=None,
+            history=cost_function.history if self.keep_value_history else [],
         )
 
     def get_values_grid(self, optimization_results: OptimizeResult) -> np.ndarray:
         """Shapes the values from the optimization results into the shape of the grid.
 
         Args:
-            optimization_results (OptimizeResult): an optimization results dictionary
-        
+            optimization_results: an optimization results dictionary
+
         Returns:
             numpy.ndarray: the values obtained at each grid point, shaped to have the same dimensions as the mesh grid
         """
---
+++ src/python/zquantum/optimizers/serialization.py
@@ -0,0 +1,38 @@
+from .grid_search import ParameterGrid
+from zquantum.core.utils import SCHEMA_VERSION
+from zquantum.core.typing import Writeable
+import json
+
+
+def save_parameter_grid(grid: ParameterGrid, filename: str) -> None:
+    """Saves a parameter grid to a file.
+
+    Args:
+        grid (core.circuit.ParameterGrid): the parameter grid to be saved
+        filename (str): the name of the file
+    """
+
+    data = grid.to_dict()
+    data["schema"] = SCHEMA_VERSION + "-parameter_grid"
+
+    with open(filename, "w") as f:
+        f.write(json.dumps(data))
+
+
+def load_parameter_grid(file: Writeable) -> ParameterGrid:
+    """Loads a parameter grid from a file.
+
+    Args:
+        file (str or file-like object): the name of the file, or a file-like object.
+
+    Returns:
+        core.circuit.ParameterGrid: the parameter grid
+    """
+
+    if isinstance(file, str):
+        with open(file, "r") as f:
+            data = json.load(f)
+    else:
+        data = json.load(file)
+
+    return ParameterGrid.from_dict(data)
