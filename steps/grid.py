9db9670805f3bc69b00eeaca6c8151ea243ee70c
Michał Stęchły <michal.stechly@gmail.com>
2021-06-02 10:07:37 -0400

    178268265 update new circuits (#57)
    * Update to new circuits.
    
    * Added debug prints.
    
    * Fixed import in grid step.

--- steps/grid.py
+++ steps/grid.py
@@ -3,7 +3,7 @@ from zquantum.core.typing import Specs
 import numpy as np
 from zquantum.core.utils import load_from_specs
 from zquantum.optimizers.grid_search import (
-    build_uniform_param_grid,
+    build_uniform_param_grid as _build_uniform_param_grid,
     save_parameter_grid,
 )
 
@@ -24,7 +24,7 @@ def build_uniform_param_grid(
     else:
         number_of_params = number_of_params_per_layer
 
-    grid = build_uniform_param_grid(
+    grid = _build_uniform_param_grid(
         number_of_params, number_of_layers, min_value, max_value, step
     )
     save_parameter_grid(grid, "parameter-grid.json")
