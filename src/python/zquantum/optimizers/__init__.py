20c2212fd1c0b9e7de7ee897eb3d1e8dcd2e7915
mstechly <michal.stechly@gmail.com>
2021-06-10 15:32:55 -0400

    Added LayerwiseAnsatzOptimizer.

--- src/python/zquantum/optimizers/__init__.py
+++ src/python/zquantum/optimizers/__init__.py
@@ -2,3 +2,4 @@ from .grid_search import GridSearchOptimizer
 from .scipy_optimizer import ScipyOptimizer
 from .cma_es_optimizer import CMAESOptimizer
 from .basin_hopping import BasinHoppingOptimizer
+from .layerwise_ansatz_optimizer import LayerwiseAnsatzOptimizer
