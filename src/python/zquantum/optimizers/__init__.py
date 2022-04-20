d5e09137073c66b09bc2e6e9bfa1418898872710
AthenaCaesura <athena.caesura@zapatacomputing.com>
2022-04-20 15:58:26 -0400

    removed imports from grid_search

--- src/python/zquantum/optimizers/__init__.py
+++ src/python/zquantum/optimizers/__init__.py
@@ -1,6 +1,8 @@
 ################################################################################
 # © Copyright 2020-2022 Zapata Computing Inc.
 ################################################################################
+from ._parameter_grid import ParameterGrid, build_uniform_param_grid
+from ._serialization import load_parameter_grid, save_parameter_grid
 from .basin_hopping import BasinHoppingOptimizer
 from .cma_es_optimizer import CMAESOptimizer
 from .grid_search import GridSearchOptimizer
--- src/python/zquantum/optimizers/grid_search/__init__.py
+++
@@ -1,6 +0,0 @@
-################################################################################
-# © Copyright 2021-2022 Zapata Computing Inc.
-################################################################################
-from ._grid_search import GridSearchOptimizer
-from ._parameter_grid import ParameterGrid, build_uniform_param_grid
-from ._serialization import load_parameter_grid, save_parameter_grid
